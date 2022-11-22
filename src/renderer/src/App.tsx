dee4f2973853977a319a21767aa9ca3aff9c2e2d
Futhememe <kyoro_souza@hotmail.com>
2022-11-22 17:47:15 -0300

    ✨ skeleton functions

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,16 @@
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
+
 export function App() {
   return (
-    <div className="h-screen w-scree text-rotion-100">
-      <h1>Ei</h1>
+    <div className="h-screen w-screen text-rotion-100 flex">
+      <Sidebar />
+      <div className="flex-1 flex flex-col max-h-screen">
+        <Header />
+        <main className="flex-1 flex items-center justify-center text-rotion-400">
+          Selecione ou crie
+        </main>
+      </div>
     </div>
   )
 }
