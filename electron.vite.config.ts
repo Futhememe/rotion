50e808ff622b26915c734f4fc9dcc837638fb43e
Futhememe <kyoro_souza@hotmail.com>
2022-11-21 23:54:35 -0300

    🎉 project config

---
+++ electron.vite.config.ts
@@ -0,0 +1,30 @@
+import { resolve } from 'path'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+  },
+  renderer: {
+    css: {
+      postcss: {
+        plugins: [
+          tailwindcss({
+            config: './src/renderer/tailwind.config.js',
+          }),
+        ],
+      },
+    },
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src'),
+      },
+    },
+    plugins: [react()],
+  },
+})
---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,3 @@
+export function App() {
+  return <h1 className="text-violet-400 font-bold text-4xl">Hello world</h1>
+}
