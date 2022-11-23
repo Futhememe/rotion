b94a77f6ddc59235f1d3f7577ef469a482bdafa6
Futhememe <kyoro_souza@hotmail.com>
2022-11-23 19:41:14 -0300

    🎨 new routes structure

--- src/main/index.ts
+++ src/main/index.ts
@@ -8,8 +8,8 @@ import { createFileRoute, createURLRoute } from 'electron-router-dom'
 function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
+    width: 1120,
+    height: 700,
     show: false,
     autoHideMenuBar: true,
     backgroundColor: '#17141f',
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,15 +1,5 @@
-import { Header } from './components/Header'
-import { Sidebar } from './components/Sidebar'
 import { Routes } from './Routes'
 
 export function App() {
-  return (
-    <div className="h-screen w-screen text-rotion-100 flex">
-      <Sidebar />
-      <div className="flex-1 flex flex-col max-h-screen">
-        <Header />
-        <Routes />
-      </div>
-    </div>
-  )
+  return <Routes />
 }
