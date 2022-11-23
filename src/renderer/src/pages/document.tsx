e741973a9ba8b86e4eaf19ec7c12a8228207541e
Futhememe <kyoro_souza@hotmail.com>
2022-11-23 19:40:53 -0300

    ✨ document page structure

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { ToC } from '../components/ToC'
 
 export const Document = () => {
   return (
-    <main className="flex-1 flex items-center justify-center text-rotion-400">
-      Document
-      <Link to="/">Acessar blank</Link>
+    <main className="flex-1 flex py-12 px-10 gap-8">
+      <aside className="hidden lg:block sticky top-0">
+        {/* eslint-disable-next-line prettier/prettier */}
+        <span className="text-rotion-300 font-semibold text-xs uppercase">Table of contents</span>
+        <ToC.Root>
+          <ToC.Link>Back-end</ToC.Link>
+          <ToC.Section>
+            <ToC.Link>banco de dados</ToC.Link>
+            <ToC.Link>Autenticacao</ToC.Link>
+          </ToC.Section>
+        </ToC.Root>
+      </aside>
+
+      <section className="flex-1 flex flex-col items-center"></section>
     </main>
   )
 }
