150eb9f4ffd22b388300cafb4f1e36ed169b44e5
gs.souza <gs.souza@einstein.br>
2023-04-22 18:20:26 -0300

    💄 update floating button

--- src/renderer/src/components/FloatingMenu/Floating/Button.tsx
+++ src/renderer/src/components/FloatingMenu/Floating/Button.tsx
@@ -4,8 +4,20 @@ interface IFloatingButton {
   title: string
   content: string
   icon: ReactNode
+  onClick?: () => void
 }
 
-export const FloatingButton = () => {
-  return <button>botao</button>
+export const FloatingButton = ({ icon, content, title, onClick }: IFloatingButton) => {
+  return (
+    <div className="flex gap-2 px-3 py-2 cursor-pointer w-full" onClick={() => onClick?.()}>
+      <div className="flex h-12 w-12 bg-white rounded-md items-center justify-center text-rotion-900 ">
+        {icon}
+      </div>
+
+      <div className="flex flex-col justify-center gap-1">
+        <h6 className="font-semibold text-rotion-50">{title}</h6>
+        <p className="text-sm font-light">{content}</p>
+      </div>
+    </div>
+  )
 }
--- src/renderer/src/components/FloatingMenu/index.tsx
+++ src/renderer/src/components/FloatingMenu/index.tsx
@@ -1,14 +1,23 @@
 import { FloatingMenu as TipFloatingMenu, FloatingMenuProps } from '@tiptap/react'
 import { Floating } from './Floating'
+import { TextHOne } from 'phosphor-react'
 
 interface IFloatingMenu extends Omit<FloatingMenuProps, 'children'> {}
 
 export const FloatingMenu = ({ editor, ...rest }: IFloatingMenu) => {
   return (
-    <TipFloatingMenu {...rest} editor={editor} tippyOptions={{ placement: 'bottom-start' }}>
+    <TipFloatingMenu
+      {...rest}
+      editor={editor}
+      tippyOptions={{ placement: 'bottom-start', delay: 600 }}
+    >
       <Floating.Root>
         <Floating.Title title="Blocos basicos" />
-        <Floating.Button />
+        <Floating.Button
+          icon={<TextHOne size={32} />}
+          title="Heading 1"
+          content="Big section heading"
+        />
       </Floating.Root>
     </TipFloatingMenu>
   )
