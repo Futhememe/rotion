9d04bc818c08441a00ee845998ecd463949fc2fd
gs.souza <gs.souza@einstein.br>
2023-04-25 13:34:06 -0300

    🎨 new logic to floating placeholder appear

--- src/renderer/src/components/FloatingPlaceholder/index.tsx
+++ src/renderer/src/components/FloatingPlaceholder/index.tsx
@@ -5,7 +5,32 @@ interface IFloatingMenu extends Omit<FloatingMenuProps, 'children'> {}
 
 export const FloatingPlaceholder = ({ editor, ...rest }: IFloatingMenu) => {
   return (
-    <FloatingMenu {...rest} className="floating-placeholder" editor={editor}>
+    <FloatingMenu
+      {...rest}
+      className="floating-placeholder"
+      editor={editor}
+      shouldShow={({ state, view }) => {
+        const { selection } = state
+        const { $anchor, empty } = selection
+        const isRootDepth = $anchor.depth === 1
+        const isEmptyAndNotHeading =
+          $anchor.parent.isTextblock &&
+          !$anchor.parent.type.spec.code &&
+          !$anchor.parent.textContent &&
+          !editor.isActive('heading')
+
+        if (
+          !view.hasFocus() ||
+          !empty ||
+          !isRootDepth ||
+          !isEmptyAndNotHeading ||
+          !editor.isEditable
+        ) {
+          return false
+        }
+        return true
+      }}
+    >
       <p className="text-xs text-rotion-300 font-medium">Pressione ' / ' para abrir o menu</p>
     </FloatingMenu>
   )
